4a747c15fbcfe6580a795b610b6d497ef34d3708
RakshithagowdaAnnaiah <annaiahrakshitha@gmail.com>
2024-11-23 13:32:05 +0000

    Add Practical7 codes

---
+++ practical7/README.md
@@ -0,0 +1,25 @@
+  GNU nano 5.6.1                                             README.md.save.1                                              Modified  #Practical7
+
+This folder contain practical 7 codes
+
+#compile
+
+gcc e.c -o eC -lm
+gcc pointers.c -o pointersC -lm
+
+#Execute
+
+./eC
+
+#Output
+
+Please enter the order you wish to use
+3
+1.0000000000000000
+0.5000000000000000
+0.1666666666666667
+e is estimated as 2.6666666667, with difference -5.161516e-02
+
+./pointersC
+
+1 1 1 1 1
---
+++ practical7/e.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int factorial(int x);
+
+int main(void)
+{
+   int i,order;
+   double e, *terms;
+   printf("Please enter the order you wish to use\n");
+   scanf("%d" , &order);
+   
+   terms = (double*)malloc(order*sizeof(double));
+   for (i=0;i<order;i++)
+   {
+      terms[i] = 1.0/(double)factorial(i+1);
+      printf("%0.16f\n" , terms[i]);
+   }
+   e = 1.0;
+   for(i=0;i<order; i++)
+   {
+      e = e + terms[i];
+   }
+   free(terms);
+   printf("e is estimated as %.10lf, with difference %e\n", e, e - exp(1.0));
+   return 0;
+}
+
+int factorial(int x){
+   if(x<0)
+      {
+      printf("you must use a positive number\n");
+      return (-1);
+      }
+      else if (x == 0)
+      {
+         return 1;
+      }
+      else
+     {
+     return (x* factorial(x-1));
+     }
+}
